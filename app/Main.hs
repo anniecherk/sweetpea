90b28dd297372057a2f9c27155073536b3960e70
Annie Cherkaev <annie.cherk@gmail.com>
2017-05-24 13:56:49 -0600

    set up skeleton for testing k of n

--- app/Main.hs
+++ app/Main.hs
@@ -6,10 +6,17 @@ import System.Environment
 import System.Directory
 import Data.Char
 
+-- TODO: refactor so doTestPopCount and doTestKofN are a single function
+-- function signature polymorphism problem
 doTestPopCount :: String -> Int -> IO String
 doTestPopCount file nSetVars= do
   result <- readFile file
   return $ validate $ testPopCountResult result nSetVars
+
+doTestKofN :: String -> Int -> Int -> IO String
+doTestKofN file k nSetVars = do
+  result <- readFile file  -- TODO CHANGE THIS FOR THE LOVE OF GOD
+  return $ validate $  ParseError--testPopCountResult result nSetVars
 
 validate :: SATResult -> String
 validate Correct = "Correct!"
@@ -44,6 +51,31 @@ splitOnArgs args
       putStrLn "Done testing"
 
 
+      ------------------------------------------------------------------------------------------------------------------------
+      -- mean to be run with the gen_popcount_results.sh script: it generates files, runs them, reads them back in to be validated
+      -- example useage: gen_popcount_results.sh 6
+      -- 6 specifies the length of the sequence we're exhausitvely validating
+      -- defaults to 2
+      -- popcount is tested by setting the inputs, and validating what they add to!
+        | head args == "generateKofN"
+        = do
+            -- let popCountLength = if length args == 2
+            --                      then read (head (tail args)) :: Int
+            --                      else 2                                                                        -- zipping index for file names
+            -- mapM_ (\(i, x) -> writeFile ("KofNTests/" ++ show popCountLength ++ "_of_" ++ show i ++ ".cnf") x) $ zip [0..] $ popCountDIMACS popCountLength
+            putStrLn "Done generating tests"
+      -----
+        | head args == "testKofN"
+        = do
+            -- fileList <- getDirectoryContents "./popCountResults"
+            -- -- tail . tail gets rid of . & .. directories
+            -- results <- mapM (\x -> doTestPopCount ("./popCountResults/" ++ x) $ (digitToInt . head) x) $ drop 2 fileList
+            -- if length args == 2 && (args !! 1) == "v"
+            -- then mapM_ putStrLn results
+            -- else mapM_ putStrLn $ filter (/="Correct!") results
+            putStrLn "Done testing"
+
+
 ------------------------------------------------------------------------------------------------------------------------
   | head args == "halfAdder" --HALF ADDER (they dont have sol's: I checked these by hand early on)
   = mapM_ (\(i, x) -> writeFile ("generated_tests/halfAdder_" ++ show i ++ ".cnf") x) $ zip [0..] testHalfAdderDIMACS
@@ -74,7 +106,8 @@ splitOnArgs args
 ------------------------------------------------------------------------------------------------------------------------
   | otherwise = putStrLn $ "I don't know what you want from me! \n You said: " ++
   show args ++ "\nTry one of these cmdln args:\n\
-  \generatePopCount [2] [v], testPopCount, halfAdder, fullAdder, rippleCarry [2] \n"
+  \generatePopCount [2], testPopCount [v], \n\
+  \generateKofN [2], testKofN [v], \nhalfAdder, fullAdder, rippleCarry [2] \n"
 
 
 
