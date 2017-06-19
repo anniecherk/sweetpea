c4415503ce88deaae728c69044304c7003f6a73f
Annie Cherkaev <annie.cherk@gmail.com>
2017-06-19 11:16:04 -0600

    set up parsing for objects as labeled trees

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,23 +1,108 @@
-{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Parser where
 
-
+import Control.Monad (mzero)
 import Data.Aeson
+import qualified Data.ByteString.Lazy as BL
 import Data.Text
-import Control.Applicative
-import Control.Monad
-import qualified Data.ByteString.Lazy as B
-import GHC.Generics
 
--- | Type of each JSON entry in record syntax.
-data Person =
-  Person { firstName  :: !Text
-         , lastName   :: !Text
-         , age        :: Int
-         , likesPizza :: Bool
-           } deriving (Show,Generic)
+data BinValTree = Zero | One | Branch [BinValTree]
+  deriving Show
 
--- Instances to convert our type to/from JSON.
+instance FromJSON BinValTree where
+  parseJSON (Number 0)  = return Zero
+  parseJSON (Number 1)  = return One
+  parseJSON a@(Array _) = Branch `fmap` parseJSON a
+  parseJSON _           = mzero
 
-instance FromJSON Person
-instance ToJSON Person
+
+
+-- ie
+-- Color
+--     Dark Colors
+--        Black
+--        Pink -- it's a really dark pink
+--     Light Colors
+--         Pink
+--         White
+-- -- this is represented as
+-- (Feature Color
+--       (NestedLabel  DarkColors (TerminalLabel Black) (TerminalLabel Pink))
+--       (NestedLabel LightColors (TerminalLabel Pink)  (TerminalLabel White)))
+--
+-- Only leaf nodes are assigned variable #'s, and we store them. --> this happens outside the DS
+-- These number will be important for propositionalizing HL constraints.
+
+-- type LevelID = Int
+
+data Feature = Feature String [Label] deriving Show
+data Label = TerminalLabel String | NestedLabel String [Label] deriving Show
+
+instance FromJSON Label where
+  parseJSON (String s) = return $ TerminalLabel (unpack s)
+  parseJSON (Object v) =
+            NestedLabel <$> v .: pack "name"
+                        <*> v .: pack "children"
+  parseJSON _ = mzero
+
+instance FromJSON Feature where
+  parseJSON (Object v) =
+            Feature <$> v .: pack "name"
+                    <*> v .: pack "children"
+  parseJSON _ = mzero
+
+
+data HL_IR = HL_IR [Feature] [RawConstraint]
+-- resolving the ints...
+data RawConstraint =
+  NoMoreThanKInARow Int Int |
+  AtLeastKInARow Int Int |
+  NoMoreThanKOutOfJ Int Int Int |
+  AtLeastKOutOfJ Int Int Int |
+  BalanceTransitions Int |
+  FullyCross [Int] Int
+
+
+
+
+
+
+
+
+
+
+
+data Example = Example String String deriving Show
+instance FromJSON Example where
+  parseJSON = withObject "tuple" $ \o -> do
+    a <- o .: pack "a"
+    b <- o .: pack "b"
+    return (Example a b)
+
+
+
+
+
+
+main :: IO ()
+main = BL.getContents >>= print . decodeObjects
+
+
+decodeObjects :: BL.ByteString -> Maybe Feature
+decodeObjects = decode
+
+-- decodeObjects :: BL.ByteString -> Maybe Feature
+-- decodeObjects = decode
+
+
+-- decodeExample :: BL.ByteString -> Maybe Example
+-- decodeExample = decode
+
+
+
+-- | Reads stdin, and parses as JSON encoded BinValTree
+-- main :: IO ()
+-- main = BL.getContents >>= print . decodeBinValTree
+--
+-- decodeBinValTree :: BL.ByteString -> Maybe BinValTree
+-- decodeBinValTree = decode
