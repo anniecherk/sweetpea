6a87dad489f651486743525bfee02ade2ec00c1f
Annie Cherkaev <annie.cherk@gmail.com>
2017-06-23 09:05:01 -0600

    wip HL_to_IL desugaring

--- src/HL_to_IL.hs
+++ src/HL_to_IL.hs
@@ -46,31 +46,56 @@ data IL_Constraint =
   IL_NoMoreThanKOutOfJ  Variables Int Int | -- rep "max in a row" here too
   IL_AtLeastKOutOfJ     Variables Int Int | -- rep "min in a row" here too
   IL_BalanceTransitions Variables Variable deriving (Show, Eq)
+--
+-- data IL_IR = IL_IR { cellLookup :: CellToVariableDict -- this is how the NAME of a cell translates to a variable
+--                    , importantVars :: Variables -- important in that these are the ones we'll care about interpretting after SAT
+--                    , levelSizes :: [Int]
+--                    , objectSize :: Int -- sum of level sizes
+--                    , levelLookup :: LevelToVariablesDict
+--                    , constraints :: [IL_Constraint]
+--                    } deriving (Show, Eq)
 
-data IL_IR = IL_IR { cellLookup :: CellToVariableDict -- this is how the NAME of a cell translates to a variable
-                   , importantVars :: Variables -- important in that these are the ones we'll care about interpretting after SAT
-                   , levelSizes :: [Int]
-                   , objectSize :: Int -- sum of level sizes
-                   , levelLookup :: LevelToVariablesDict
-                   , constraints :: [IL_Constraint]
-                   } deriving (Show, Eq)
+type IL_IR = [IL_Constraint]
 
 
+translate_HL_to_IL :: HL_IR -> IL_IR
+translate_HL_to_IL (HL_IR factorPaths whatToCross rawConstraints) =
+  map (\x -> getIL_Constraint x levelLookUp) rawConstraints
+  where levelSizes = getLevelSizes factorPaths whatToCross
+        objectSize = getObjectSize levelSizes
+        importantVars = getImportantVars objectSize
+        cellLookUp = getCellLookUp factorPaths importantVars
+        -- CellToVariableDict -> LevelToVariablesDict
+        levelLookUp = getLevelLookUp factorPaths cellLookUp
 
+-- for fully crossing
+-- will we ever fully cross on anything other than the factor level?
+-- assuming no- but
+-- TODO: the fullycrossed datastructure is set up for "yes"
 getLevelSizes :: FactorPaths -> FullyCross -> [Int]
-getLevelSizes allPaths whatToCross = [0]
+getLevelSizes allPaths (FullyCross toCross _) =
+  map (\y -> length $ filter id $ map (\x -> head x == head y) allPaths) toCross
+-- for unit testing:
+-- allPaths = [["color","darkColors","black"],["color","darkColors","pink"], ["color","lightColors","white"],["color","lightColors","pink"],["shape","circle"], ["shape","square"],["shape","triangle"]]
+-- toCross = [["shape"],["color"]]
 
+-- allPaths2 = [["color","darkColors","black"],["color","darkColors","pink"],["color","lightColors","white"],["color","lightColors","pink"],["shape","circle"],["shape","square"],["shape","triangle"],["temperature","warm"],["temperature","cold"]]
+
+-- this defines a "unit"
 getObjectSize :: [Int] -> Int
 getObjectSize = sum
 
+-- important in that these are the ones we'll care about interpretting after SAT
 getImportantVars :: Int -> Variables
 getImportantVars objectSize = [1..objectSize]
 
+-- this is how the NAME of a cell translates to a variable
 getCellLookUp :: FactorPaths -> Variables -> CellToVariableDict
 getCellLookUp allPaths importantVars = M.empty
 
---TODO: Maybe this should take paths ref'd in HL_constraints?
-getLevelLookUp :: FactorPath -> Variables -> LevelToVariablesDict
+-- a dictionary of all partial paths to all the variables they map to
+-- TODO: partial paths: I think what we actually mean is an "OR"
+getLevelLookUp :: FactorPaths -> CellToVariableDict -> LevelToVariablesDict
 getLevelLookUp allPaths importantVars = M.empty
 
 --This is the heavy lifting
