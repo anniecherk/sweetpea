dfc2a702ef14273f2945ca6994d3853c248f6711
Annie Cherkaev <annie.cherk@gmail.com>
2017-06-23 12:54:14 -0600

    finished il_to_ll, but need to test both desugaring passes

--- src/IL_to_LL.hs
+++ src/IL_to_LL.hs
@@ -6,6 +6,7 @@ where
 import Parser
 import HL_to_IL
 import qualified Data.Map as M
+import Data.List (tails)
 
 
 -- Translate from:
@@ -24,9 +25,30 @@ import qualified Data.Map as M
 
 
 
-data CompilerOp = LessThan | GreaterThan | Exactly
+data CompilerOp = LessThan Int | GreaterThan Int | Exactly Int
 type LL_Constraint = (CompilerOp, Variables)
 type LL_IR =[LL_Constraint]
 
+
+-- data IL_Constraint =
+--   IL_AssertCorrectAmount    Variables Int | --this is for like "we have 3 red balls"
+--   IL_NoMoreThanKOutOfJ  Variables Int Int | -- rep "max in a row" here too
+--   IL_AtLeastKOutOfJ     Variables Int Int | -- rep "min in a row" here too
+--   IL_BalanceTransitions Variables Variable deriving (Show, Eq)
+-- --
+-- TODO: handle balance transitions bc idk what to do now
 desugarConstraints :: IL_Constraint -> LL_IR
-desugarConstraints il_constraint = [(LessThan, [0])]
+desugarConstraints (IL_AssertCorrectAmount vars k) = [(Exactly k, vars)]
+desugarConstraints (IL_NoMoreThanKOutOfJ vars k j) =
+  map (\x-> (GreaterThan k, x)) $ gather j vars
+desugarConstraints (IL_AtLeastKOutOfJ vars k j)    =
+  map (\x-> (LessThan k, x)) $ gather j vars
+
+
+il_to_ll :: IL_IR -> LL_IR
+il_to_ll = concatMap desugarConstraints
+
+-- thanks stackoverflow
+-- https://stackoverflow.com/questions/24599875/is-there-a-built-in-function-to-get-all-consecutive-subsequences-of-size-n-of-a
+gather :: Int -> Variables -> [Variables]
+gather n inputList = takeWhile ((== n) . length) $ map (take n) $ tails inputList
