a8bf444c0162815d1935b3468cec6690aee88476
Annie Cherkaev <annie.cherk@gmail.com>
2017-06-23 12:26:27 -0600

    wip hl to il, done with constraints, need to add 'exactly number' desugaring

--- src/HL_to_IL.hs
+++ src/HL_to_IL.hs
@@ -6,6 +6,7 @@ where
 
 import Parser
 import qualified Data.Map as M
+import Data.Maybe
 
 
 -- get something like
@@ -17,9 +18,9 @@ import qualified Data.Map as M
 --
 --   (FullyCross [["shape"],["color"]] 2)
 --
---   [NoMoreThanKOutOfJ [["color","lightColors","pink"]] 3 4
---   , AtLeastKOutOfJ [["shape","circle"]] 2 7,
---   NoMoreThanKInARow [["color","lightColors","pink"]] 3]
+  -- [NoMoreThanKOutOfJ [["color","lightColors","pink"]] 3 4
+  -- , AtLeastKOutOfJ [["shape","circle"]] 2 7,
+  -- NoMoreThanKInARow [["color","lightColors","pink"]] 3]
 
 -- need to make a list of [cnf] variables of the right length
 
@@ -38,8 +39,8 @@ type LevelCollection = [Variable]
 
 -- maybe this is in the next step?
 -- note: lists of strings is a bad key urgh
-type CellToVariableDict = M.Map FactorPaths Variable
-type LevelToVariablesDict = M.Map FactorPaths Variables
+type CellToVariableDict = M.Map FactorPath Variable
+type LevelToVariablesDict = M.Map FactorPath Variables
 
 data IL_Constraint =
   IL_AssertCorrectAmount    Variables Int | --this is for like "we have 3 red balls"
@@ -59,14 +60,23 @@ type IL_IR = [IL_Constraint]
 
 
 translate_HL_to_IL :: HL_IR -> IL_IR
-translate_HL_to_IL (HL_IR factorPaths whatToCross rawConstraints) =
-  map (\x -> getIL_Constraint x levelLookUp) rawConstraints
-  where levelSizes = getLevelSizes factorPaths whatToCross
+translate_HL_to_IL (HL_IR factorPaths fullyCross rawConstraints) =
+  concatMap (`getIL_Constraint` levelLookUp) rawConstraints
+  where levelSizes = getLevelSizes factorPaths fullyCross
         objectSize = getObjectSize levelSizes
         importantVars = getImportantVars objectSize
         cellLookUp = getCellLookUp factorPaths importantVars
+        numObjects = totalNumberObjects objectSize fullyCross
         -- CellToVariableDict -> LevelToVariablesDict
-        levelLookUp = getLevelLookUp factorPaths cellLookUp
+        levelLookUp = getLevelLookUp factorPaths numObjects importantVars
+        --TODO!!! also "assert correct number" constraints!!
+
+
+        -- getLevelLookUp :: FactorPaths -> Int -> CellToVariableDict -> LevelToVariablesDict
+        -- getLevelLookUp allPaths numObjects importantVars = M.empty
+        --
+        -- --This is the heavy lifting
+        -- getIL_Constraint :: RawConstraint -> LevelToVariablesDict -> IL_Constraint
 
 -- for fully crossing
 -- will we ever fully cross on anything other than the factor level?
@@ -89,15 +99,68 @@ getObjectSize = sum
 getImportantVars :: Int -> Variables
 getImportantVars objectSize = [1..objectSize]
 
+totalNumberObjects :: Int -> FullyCross -> Int
+totalNumberObjects objectSize (FullyCross _ numReps) = objectSize * numReps
+
 -- this is how the NAME of a cell translates to a variable
+-- TODO: partial paths? for later
+-- TODO: either use in getLevelLookup or cut this function
 getCellLookUp :: FactorPaths -> Variables -> CellToVariableDict
-getCellLookUp allPaths importantVars = M.empty
+getCellLookUp allPaths importantVars = M.fromList $ zip allPaths importantVars
+
+
+
+-- allPaths = [["color","darkColors","black"],["color","darkColors","pink"], ["color","lightColors","white"],["color","lightColors","pink"],["shape","circle"], ["shape","square"],["shape","triangle"]]
+-- importantVars = [1..7]
+
 
 -- a dictionary of all partial paths to all the variables they map to
--- TODO: partial paths: I think what we actually mean is an "OR"
-getLevelLookUp :: FactorPaths -> CellToVariableDict -> LevelToVariablesDict
-getLevelLookUp allPaths importantVars = M.empty
+-- value is a LIST of variables (as opposed to getCellLookup where value is a VARIABLE)
+-- TODO: partial paths: I think what we actually mean is an "OR" so wait on it
+-- switch to CellToVariableDict for partial paths?
+getLevelLookUp :: FactorPaths -> Int -> Variables -> LevelToVariablesDict
+getLevelLookUp allPaths numObjects importantVars = M.fromList $ zip allPaths levels
+  where objectSize = length importantVars
+        levels = map (\x -> [x,(x+objectSize)..(numObjects*objectSize)]) importantVars
+
+--HACK: good god fix this
+unwrapLevel :: FactorPath -> LevelToVariablesDict -> Variables
+unwrapLevel applyPath levelLookUp = fromMaybe [] ( M.lookup applyPath levelLookUp)
+
+  -- case M.lookup applyPath levelLookUp of
+  -- Just vars -> vars
+  -- Nothing -> []
+
+
 
 --This is the heavy lifting
-getIL_Constraint :: RawConstraint -> LevelToVariablesDict -> IL_Constraint
-getIL_Constraint rawConstraint levelLookUp = IL_AssertCorrectAmount [0] 0
+-- [NoMoreThanKOutOfJ [["color","lightColors","pink"]] 3 4
+-- , AtLeastKOutOfJ [["shape","circle"]] 2 7,
+-- NoMoreThanKInARow [["color","lightColors","pink"]] 3]
+-- need to return IL_Constraint *List* because we allow shorthand of applying constraints over many levels
+getIL_Constraint :: RawConstraint -> LevelToVariablesDict -> [IL_Constraint]
+getIL_Constraint (NoMoreThanKInARow applyPaths k) levelLookUp
+  = map (\x-> IL_NoMoreThanKOutOfJ (unwrapLevel x levelLookUp) k k) applyPaths
+getIL_Constraint (AtLeastKInARow     applyPaths k) levelLookUp
+  = map (\x-> IL_AtLeastKOutOfJ (unwrapLevel x levelLookUp) k k) applyPaths
+getIL_Constraint (NoMoreThanKOutOfJ  applyPaths k j) levelLookUp
+  = map (\x-> IL_NoMoreThanKOutOfJ (unwrapLevel x levelLookUp) k j) applyPaths
+getIL_Constraint (AtLeastKOutOfJ     applyPaths k j) levelLookUp
+  = map (\x-> IL_AtLeastKOutOfJ (unwrapLevel x levelLookUp) k j) applyPaths
+-- getIL_Constraint (BalanceTransitions applyPath) levelLookUp = IL_NoMoreThanKOutOfJ [0] 0 0 TODO: transitions are hard
+
+
+--
+-- data IL_Constraint =
+--   IL_AssertCorrectAmount    Variables Int | --this is for like "we have 3 red balls"
+--   IL_NoMoreThanKOutOfJ  Variables Int Int | -- rep "max in a row" here too
+--   IL_AtLeastKOutOfJ     Variables Int Int | -- rep "min in a row" here too
+--   IL_AtLeastKOutOfJ Variables Variable deriving (Show, Eq)
+
+-- -- resolving the ints...
+-- data RawConstraint =
+--   NoMoreThanKInARow  FactorPaths Int |
+--   AtLeastKInARow     FactorPaths Int |
+--   NoMoreThanKOutOfJ  FactorPaths Int Int |
+--   AtLeastKOutOfJ     FactorPaths Int Int |
+--   BalanceTransitions FactorPaths
