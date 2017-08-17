824351fef038c8d7de1f6e3a15f8d7c8c0da9a98
Annie Cherkaev <annie.cherk@gmail.com>
2017-08-17 16:08:58 -0600

    added tests for utilities in datastructures

--- test/Spec.hs
+++ test/Spec.hs
@@ -17,22 +17,63 @@ main = defaultMain tests --putStrLn "not set up"
 
 
 tests :: TestTree
-tests = testGroup "Tests" [hlToIlTests]
+tests = testGroup "Tests" [hlToIlTests, dataStructureTests]
 
 hlToIlTests :: TestTree
 hlToIlTests = testGroup "HLtoIL Tests" [oneHotTests]
 
+dataStructureTests :: TestTree
+dataStructureTests = testGroup "DataStructure Tests" [helperTests]
 
 ---------------------------------------------------------------------------------------------------------------
 -- HL to IL Tests
 
 oneHotTests = testGroup "one hot tests"
   [ testCase "empty edgecase" $
-      execState (enforceOneHot []) (initState 0)
+      execState (enforceOneHot []) emptyState
         @?= (0,[[]])
   ,  testCase "small example: 2 elements" $
       execState (enforceOneHot [1..2]) (initState 2)
         @?= (2,[[-1,-2],[1,2]])
+  ,  testCase "small example: 3 elements" $
+      execState (enforceOneHot [1..3]) (initState 3)
+        @?= (3,[[-1,-2],[-1,-3],[-2,-3],[1,2,3]])
+  ,  testCase "4 elements + indep of variables & cnf" $
+      execState (enforceOneHot [1..4]) (initState 6)
+        @?= (6,[[-1,-2],[-1,-3],[-1,-4],[-2,-3],[-2,-4],[-3,-4],[1,2,3,4]]) -- TODO: check against cryptosatmini
+  ]
+
+
+-- DataStructures tests
+
+helperTests = testGroup "var/cnf utility tests"
+  [ testCase "getFresh" $
+      runState getFresh emptyState
+        @?= (1,(1,[]))
+  , testCase "getNFresh" $
+      runState (getNFresh 3) emptyState
+        @?= ([1,2,3],(3,[]))
+  , testCase "nested getNFresh" $
+      runState (getNFresh 3) (execState (getNFresh 3) emptyState)
+        @?= ([4,5,6],(6,[]))
+  , testCase "appendCNF" $
+      execState (appendCNF [[1, 2, -3]]) emptyState
+        @?= (0,[[1,2,-3]])
+  , testCase "nested appendCNF" $
+      execState (appendCNF [[-4, 5], [4]]) (execState (appendCNF [[1, 2, -3]]) emptyState)
+        @?= (0,[[-4,5],[4],[1,2,-3]])
+  , testCase "zeroOut" $
+        execState (zeroOut [1, 2, 3]) emptyState
+          @?= (0,[[-1],[-2],[-3]])
+  , testCase "setToOne" $
+        execState (setToOne 1) emptyState
+          @?= (0,[[1]])
+  , testCase "setToOne + zeroOut nested" $
+        execState (zeroOut [1, 2, 3]) (execState (setToOne 4) emptyState)
+          @?= (0,[[-1],[-2],[-3],[4]])
+  , testCase "setToZero" $
+        execState (setToZero 1) emptyState
+          @?= (0,[[-1]])
   ]
 
 
