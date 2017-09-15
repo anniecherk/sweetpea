5aa0cc19310160a32eb2f57704ee279828df205c
Annie Cherkaev <annie.cherk@gmail.com>
2017-09-14 22:00:03 -0600

    first iteration of front end done!!!

--- notes/Frontend_scratch.hs
+++ notes/Frontend_scratch.hs
@@ -9,7 +9,7 @@
 import Control.Monad.Trans.State
 import Control.Monad (replicateM)
 -------------------------------------------
-import Data.List (transpose)
+import Data.List (transpose, nub)
 
 
 data HLBlock = HLBlock { hlnumTrials :: Int
@@ -46,8 +46,7 @@ data LLConstraint = AssertEq Int [Var] | AssertLt Int [Var]
                  | Entangle Var [Var] deriving(Show)
 
 -- list of IL blocks : get the total num allocated by looking at last block
-type LLBlock = [LLConstraint]
-type LLAST = [LLBlock]
+type LLAST = [LLConstraint]
 
 
 
@@ -61,7 +60,8 @@ type LLAST = [LLBlock]
 -- shape = NTNode "shape" [(LeafNode "circle"), (LeafNode "square")]
 -- design = [color, shape]
 -- block = makeBlock (fullyCrossSize design) [color, shape] [FullyCross]
--- ast = [block, block]
+-- ast = [block]
+-- execState (hlToIl ast >>= ilToll >>= buildCNF) emptyState
 -- runState (hlToIl ast >>= ilToll) emptyState
 -- runState (hlToIl ast) emptyState
 --
@@ -112,12 +112,14 @@ ilToll ilast = do llblocks <- mapM ilBlockToLLBlocks ilast
                   return $ concat llblocks
 
 
-ilBlockToLLBlocks :: ILBlock -> State (Count, CNF) [LLBlock]
-ilBlockToLLBlocks block@(ILBlock _ _ _ _ constraints) = mapM (\x -> desugarConstraint x block) constraints
+ilBlockToLLBlocks :: ILBlock -> State (Count, CNF) LLAST
+ilBlockToLLBlocks block@(ILBlock _ _ _ _ constraints) = do
+  llblocks <- mapM (\x -> desugarConstraint x block) constraints
+  return $ concat llblocks
 
 
 -- TODO: desugar the other constraints from HLConstraints to LLConstraints using Design
-desugarConstraint :: HLConstraint -> ILBlock -> State (Count, CNF) LLBlock
+desugarConstraint :: HLConstraint -> ILBlock -> State (Count, CNF) [LLConstraint]
 desugarConstraint Consistency inBlock = return $ trialConsistency inBlock
 desugarConstraint FullyCross  inBlock = llfullyCross inBlock
 desugarConstraint _ inBlock = error "desugar const not implemented yet"
@@ -160,9 +162,24 @@ getShapedLevels (ILBlock numTrials start end design _) = levelGroups
         trialShape = map countLeaves design
         levelGroups = map (\x -> getShapeVars x trialShape) [start, (trialSize+1).. end]
 
+-- Transformation time!
+buildCNF :: LLAST -> State (Int, CNF) ()
+buildCNF = mapM_ buildCommand
+
+buildCommand :: LLConstraint -> State (Int, CNF) ()
+buildCommand (OneHot todo) = enforceOneHot todo
+buildCommand (Entangle state levels) = aDoubleImpliesList state levels
+buildCommand _ = error "other commands not implemented"
 
 
-
+-- mutually exclusive fields actually
+-- asserts that only one of the list can be true
+-- (a and -b and -c) or (-a and b and c) or (-a and -b and c)... but in CNF
+-- which is: (a or b or c) and (-a or -b) and (-b or -c) and (-a or -c)
+enforceOneHot :: [Var] -> State (Count, CNF) ()
+enforceOneHot inList = do appendCNF [inList] -- this appends (a or b or c)
+                          appendCNF $ not_pairs inList -- appends the (-a or -b) and (-b or -c) etc pairs
+  where not_pairs xs = nub [[-x, -y] | x <- xs, y <- xs, x < y]
 
 
 
@@ -195,6 +212,19 @@ appendCNF :: CNF -> State (Count, CNF) ()
 appendCNF newEntry = do (x, accum) <- get
                         put (x, newEntry ++ accum)
                         return ()
+
+
+-- the double-implication generalization
+-- https://www.wolframalpha.com/input/?i=CNF+A++%3C%3D%3E+(B+%26%26+C+%26%26+D+%26%26+E)
+-- CNF A  <=> (B && C && D)
+-- (-a or b)
+-- (-a or c)
+-- (-a or d)
+-- (a or -b or -c or -d)
+aDoubleImpliesList :: Var -> [Var] -> State (Count, CNF) ()
+        -- makes the list of [-b, -c, .., a] : makes the lists [-a, b], [-a, c], ...
+aDoubleImpliesList a inList = appendCNF result
+  where result = (map ((-1) *) inList ++ [a]) : map (\x -> [-a, x]) inList
 
 type Count = Int
 type Var = Int
